44bb1b75955028e0db11a16b5706d254b23afa5d
Aleksandr Titov <altitov@ptsecurity.com>
2025-12-22 23:01:20 +0300

    feat(iter8): Add test for mw func TestMiddlewareGzipRead

--- internal/handler/mw_gzip_test.go
+++ internal/handler/mw_gzip_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"compress/gzip"
 	"github.com/stretchr/testify/assert"
 	"io"
@@ -42,4 +43,46 @@ func Test_MiddlewareGzipWrite(t *testing.T) {
 
 	// Проверяем заголовок Content-Encoding
 	assert.Equal(t, "gzip", w.Header().Get("Content-Encoding"))
+}
+
+func TestMiddlewareGzipRead(t *testing.T) {
+	// Тестовые данные
+	inData := `{"url":"http://test.aa"}`
+
+	// Создаем буфер в которую будут записываться сжатые данные
+	var b bytes.Buffer
+
+	// Создаем gzip writer для записи данных в буфер
+	gzw := gzip.NewWriter(&b)
+
+	// Записываем тестовые данные в gzip writer, они будут храниться в буфере
+	_, err := gzw.Write([]byte(inData))
+	assert.NoError(t, err)
+
+	// Закрываем gzip writer
+	err = gzw.Close()
+	assert.NoError(t, err)
+
+	// Создаем Handler
+	testHandler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// Читаем тело запроса
+		body, err := io.ReadAll(r.Body)
+		assert.NoError(t, err)
+
+		// Проверяем, что данные распаковались
+		assert.Equal(t, inData, string(body))
+		t.Logf("Данные запроса: %s, полученые данные %s", inData, string(body))
+	})
+
+	// Создаем Request
+	req := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewReader(b.Bytes()))
+	// Устанавливаем заголовки
+	req.Header.Set("Content-Encoding", "gzip")
+	req.Header.Set("Content-Type", "application/json")
+
+	// Создаем Recorder
+	w := httptest.NewRecorder()
+
+	// Запускаем Handler с использованием MW функции распаковывающей запрос
+	MiddlewareGzipRead(testHandler).ServeHTTP(w, req)
 }
