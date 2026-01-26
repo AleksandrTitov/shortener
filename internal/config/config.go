815c23fc9e92e5a59954d481600359686689a84e
Aleksandr Titov <altitov@ptsecurity.com>
2026-01-26 23:41:36 +0300

    feat(iter14): Add JWTSecret in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,12 +11,14 @@ type Config struct {
 	LogLevel    string
 	FileName    string
 	DatabaseDSN string
+	JWTSecret   string
 }
 
 const (
-	defaultAddr     = "localhost:8080"
-	defaultBaseHTTP = "http://localhost:8080"
-	defaultLogLevel = "debug"
+	defaultAddr      = "localhost:8080"
+	defaultBaseHTTP  = "http://localhost:8080"
+	defaultLogLevel  = "debug"
+	defaultJWTSecret = "im_not_a_secret_plz_replace_me"
 )
 
 func NewConfig() *Config {
@@ -27,6 +29,7 @@ func NewConfig() *Config {
 	flag.StringVar(&config.LogLevel, "l", defaultLogLevel, "Уровень логирования")
 	flag.StringVar(&config.FileName, "f", "", "JSON файл с данными")
 	flag.StringVar(&config.DatabaseDSN, "d", "", "Адрес подключения к базе данных")
+	flag.StringVar(&config.JWTSecret, "t", defaultJWTSecret, "Секретный ключ JWT")
 
 	flag.Parse()
 
