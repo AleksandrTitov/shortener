78d98d6fe5b705adb2a090dbe40fd820fc2d0aea
Aleksandr Titov <altitov@ptsecurity.com>
2026-02-03 09:53:54 +0300

    fix(iter15): Fix db migrations

--- migrations/migrations/000004_create_deleted_flag_row.up.sql
+++ migrations/migrations/000004_create_deleted_flag_row.up.sql
@@ -1,2 +1,5 @@
 -- Создание столбца deleted_flag
-alter table public.shorter add column deleted_flag bool;
+alter table public.shorter add column deleted_flag bool default false;
+
+-- Обновляем существующие записи
+update public.shorter set deleted_flag = false where deleted_flag is null;
